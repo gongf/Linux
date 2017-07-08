d5cd16e44972bc98917fa7ba12f44d6c18ec1074
urhosts <hosts@gsxio.com>
2017-07-08 23:53:58 +0800

    client

--- TCP_SERVER/Simple_TCP/client.c
+++ TCP_SERVER/Simple_TCP/client.c
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    //建立客户机套接字
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if(sock < 0)
     {
@@ -32,28 +33,34 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+    //创建接收的服务器套接字
     struct sockaddr_in local;
     local.sin_family = AF_INET;
     local.sin_port = htons(atoi(argv[2]));
     local.sin_addr.s_addr = inet_addr(argv[1]);
-
+    
+    //进行连接操作
     if(connect(sock, (struct sockaddr*)&local, sizeof(local)) < 0)
     {
         perror("connect");
         exit(2);
     }
 
+    //客户机进行操作
     char buf[1024];
     while(1)
     {
         printf("send:");
         fflush(stdout);
+        //客户端进行发送数据给服务器
         ssize_t s = read(0, buf, sizeof(buf) - 1);
-        buf[s - 1] = 0;
+        buf[s - 1] = 0;    
         write(sock, buf, s - 1);
+        //读取服务器端的回写消息
         read(sock, buf, sizeof(buf) - 1);
         printf("server echo: %s\n", buf);
     }
+    //关闭套接字
     close(sock);
     return 0;
 }
